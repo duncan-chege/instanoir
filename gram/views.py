c4574fbba5d4957e4811f75902139990806f1bcc
dunyung1 <dshege4@gmail.com>
2019-03-09 23:04:53 +0300

    delete signup view in project app

--- gram/views.py
+++ gram/views.py
@@ -6,16 +6,3 @@ from django.shortcuts import render, redirect
 def home(request): 
     return render(request, 'home.html')
 
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('landing')
-    else:
-        form = UserCreationForm()
-    return render(request, 'registration/signup.html', {'form': form})
