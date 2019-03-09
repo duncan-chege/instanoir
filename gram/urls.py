a05ff31be92901490d59824186e3bf2bc961d885
dunyung1 <dshege4@gmail.com>
2019-03-09 22:42:55 +0300

    create urls for login and logout

--- gram/urls.py
+++ gram/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.home, name = 'home'),
-    url(r'^signup/$', views.signup, name = 'signup')
+    url('^$',views.home, name = 'home')
 ]
--- instanoir/urls.py
+++ instanoir/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.contrib import admin
 from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'', include('gram.urls')),
-    url(r'^register', user_views.register, name = 'register')
+    url(r'^admin/', admin.site.urls),
+    url(r'^register', user_views.register, name = 'register'),
+    url(r'^login/', auth_views.loginView.as_view(template_name='users/login.html'), name = 'login'),
+    url(r'^logout/', auth_views.logoutView.as_view(template_name='users/logout.html'), name = 'logout')
 ]
