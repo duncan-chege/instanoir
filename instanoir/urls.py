ce8ebe6bff387f1cb69c6e313a2e96152df8ab5d
dunyung1 <dshege4@gmail.com>
2019-03-09 20:55:42 +0300

    add a new register path in urls file

--- instanoir/urls.py
+++ instanoir/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from users import views as user_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('gram.urls')),
-    # url(r'^accounts/', include('registration.backends.simple.urls'))
+    url(r'^register', user_views.register, name = 'register')
 ]
