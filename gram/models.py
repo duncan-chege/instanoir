98b2e9abc60f19908ade24fada744c6f38e27d57
dunyung1 <dshege4@gmail.com>
2019-03-10 08:19:27 +0300

    make Image model properties

--- gram/models.py
+++ gram/models.py
@@ -1,2 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Image(models.Model):
+    image_name = models.CharField(max_length=100)
+    image_caption = models.CharField(max_length=200)          
+    noir_image = models.ImageField(upload_to = 'photos/')
+    like = models.IntegerField(default=0)
+    comment = models.TextField()
+    grammer = models.ForeignKey(User, on_delete=models.CASCADE)     #models.CASCADE-if a user is deleted, delete their posts as well
