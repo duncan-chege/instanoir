42dcefc6a112b29db8b6d384893551babafab036
dunyung1 <dshege4@gmail.com>
2019-03-10 07:39:49 +0300

    delete model User

--- gram/models.py
+++ gram/models.py
@@ -1,8 +1,2 @@
 from django.db import models
 
-class User(models.Model):
-    username = models.CharField(max_length=20)
-    email = models.CharField(max_length=140)
-    password = models.CharField(max_length=100)
-    # bio
-    # profile_pic
