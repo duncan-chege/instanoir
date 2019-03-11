f273dec33e25b3e3040d3355288996ead4a4b703
dunyung1 <dshege4@gmail.com>
2019-03-12 02:15:48 +0300

    add django signals

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)     #one to one relationsip gives user ablity to access the profile directly from the user
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     bio =  models.CharField(max_length=100,default='No bio available')         
 
